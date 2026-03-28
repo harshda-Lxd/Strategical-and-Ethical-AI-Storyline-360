63d528f3e69371b5f037206d7bedc1c621fb3be5
harshda-Lxd <ksd.harshda@gmail.com>
2026-03-28 07:08:26 -0700

    Add files via upload

---
+++ triggers.js
@@ -0,0 +1,49 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6jBMdsDojub":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  const target = object('6Zxf8aRRftn');
+const duration = 1250;
+const easing = 'ease-out';
+const id = '6rdthoIdHR5';
+player.addForTriggers(
+id,
+target.animate(
+[ {opacity: 1 }, 
+{opacity: 0 }, 
+{opacity: 1 }, 
+{opacity: 0 }, 
+{opacity: 1 } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
